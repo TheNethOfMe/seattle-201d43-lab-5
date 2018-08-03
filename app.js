05675cea2e82d3699d853f3055d04e499e028f52
neth <nethwebdev@gmail.com>
2018-08-03 15:16:27 -0700

    logic added for question five

--- app.js
+++ app.js
@@ -102,11 +102,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  let total = 1;
+  multArr.forEach((num) => {
+    total = multiply(num, total)[0];
+  });
+  const response = `The numbers ${multArr.join(',')} have a product of ${total}.`;
+  return [total, response];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
