67af2bf1bb2674930678e8c446bae1468dfe5800
neth <nethwebdev@gmail.com>
2018-08-03 15:08:04 -0700

    added logic for question four

--- app.js
+++ app.js
@@ -76,12 +76,17 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let total = 0;
+  sumArr.forEach((num) => {
+    total = sum(num, total)[0];
+  });
+  const response = `${sumArr.join(',')} was passed in as an array of numbers, and ${total} is their sum.`;
+  return [total, response];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
